21e5a044d6ae491d43d62ac394d5a893c546ce0e
FarmVivi <farmvividev@gmail.com>
2020-07-04 13:57:55 +0200

    panel connecting logs

--- src/main/java/net/samagames/hydroangeas/client/panel/PanelController.java
+++ src/main/java/net/samagames/hydroangeas/client/panel/PanelController.java
@@ -2,14 +2,24 @@ package net.samagames.hydroangeas.client.panel;
 
 import com.mattmalec.pterodactyl4j.PteroBuilder;
 import com.mattmalec.pterodactyl4j.entities.PteroAPI;
+import net.samagames.hydroangeas.Hydroangeas;
+
+import java.util.logging.Level;
 
 public class PanelController {
-    private final PteroAPI adminPanel;
-    private final PteroAPI userPanel;
+    private PteroAPI adminPanel;
+    private PteroAPI userPanel;
 
     public PanelController(String panelUrl, String adminToken, String userToken) {
-        this.adminPanel = new PteroBuilder().setApplicationUrl(panelUrl).setToken(adminToken).build();
-        this.userPanel = new PteroBuilder().setApplicationUrl(panelUrl).setToken(userToken).build();
+        Hydroangeas.getLogger().log(Level.INFO, "Connecting to panel...");
+        try {
+            this.adminPanel = new PteroBuilder().setApplicationUrl(panelUrl).setToken(adminToken).build();
+            this.userPanel = new PteroBuilder().setApplicationUrl(panelUrl).setToken(userToken).build();
+            Hydroangeas.getLogger().log(Level.INFO, "Connected to panel.");
+        } catch (Exception e) {
+            Hydroangeas.getLogger().log(Level.SEVERE, "Can't connect to panel!");
+            e.printStackTrace();
+        }
     }
 
     public PteroAPI getAdminPanel() {
